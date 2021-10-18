d98d03c52bf95ac5ac51ad7976201034726d15b3
Edward A Maxedon, Sr <edadma@gmail.com>
2021-10-18 13:27:11 -0400

    bump version to 0.1.1

--- shared/src/test/scala/io/github/edadma/recognizer/Main.scala
+++ shared/src/test/scala/io/github/edadma/recognizer/Main.scala
@@ -8,40 +8,40 @@ object Main extends App with Testing {
   case class Image(text: String, url: String, title: Option[String])
   case class ImageLink(text: String, img: String, title: Option[String], url: String)
 
-//  val s = "[Example](https://example.com \"Example Title\")"
-////  val s = "![Image](/path/to/image.png)"
-////  val s = "[![Image](/path/to/image.png \"Example Title\")](https://example.com)"
-//  val space = rep(whitespace)
-//  val image: Pattern = "![" ~ string(rep(noneOf(']'))) ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ space ~ opt(
-//    '"' ~ string(rep(noneOf('"'))) ~ '"',
-//    1)(_.head) ~ ')' ~ action3(Image)
-//  val link: Pattern = '[' ~ string(rep(noneOf(']'))) ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ space ~ opt(
-//    '"' ~ string(rep(noneOf('"'))) ~ '"',
-//    1)(_.head) ~ ')' ~ action3(Link)
-//  val imageLink: Pattern =
-//    '[' ~ "![" ~ string(rep(noneOf(']'))) ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ space ~ opt(
-//      '"' ~ string(rep(noneOf('"'))) ~ '"',
-//      1)(_.head) ~ ')' ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ ')' ~ action4(ImageLink)
-//
-//  println(parse(s, image | link | imageLink))
+  val s = "[Example](https://example.com \"Example Title\")"
+//  val s = "![Image](/path/to/image.png)"
+//  val s = "[![Image](/path/to/image.png \"Example Title\")](https://example.com)"
+  val space = rep(whitespace)
+  val image: Pattern = "![" ~ string(rep(noneOf(']'))) ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ space ~ opt(
+    '"' ~ string(rep(noneOf('"'))) ~ '"',
+    1)(_.head) ~ ')' ~ action3(Image)
+  val link: Pattern = '[' ~ string(rep(noneOf(']'))) ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ space ~ opt(
+    '"' ~ string(rep(noneOf('"'))) ~ '"',
+    1)(_.head) ~ ')' ~ action3(Link)
+  val imageLink: Pattern =
+    '[' ~ "![" ~ string(rep(noneOf(']'))) ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ space ~ opt(
+      '"' ~ string(rep(noneOf('"'))) ~ '"',
+      1)(_.head) ~ ')' ~ ']' ~ '(' ~ string(rep(noneOf(')', '"'))) ~ ')' ~ action4(ImageLink)
+
+  println(parse(s, image | link | imageLink))
 
 //  runlimit = 20
 
-  run(StringInput("ababab"), string(rep("ab")) ~ string(rep(any))) match {
-    case None => println("no match")
-    case Some(r) =>
-      result(r)
-
-      @tailrec
-      def result(r: (Option[Any], I, Runstate)): Unit =
-        r match {
-          case (v, u, s) =>
-            println(v, u)
-            rerun(s) match {
-              case Some(r) => result(r)
-              case None    => println("no more matches")
-            }
-        }
-  }
+//  run(StringInput("ababab"), string(rep("ab")) ~ string(rep(any))) match {
+//    case None => println("no match")
+//    case Some(r) =>
+//      result(r)
+//
+//      @tailrec
+//      def result(r: (Option[Any], I, Runstate)): Unit =
+//        r match {
+//          case (v, u, s) =>
+//            println(v, u)
+//            rerun(s) match {
+//              case Some(r) => result(r)
+//              case None    => println("no more matches")
+//            }
+//        }
+//  }
 
 }
