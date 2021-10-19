c1c6c5025494c3a658081bb8e1b8daa8468067dc
Edward A Maxedon, Sr <edadma@gmail.com>
2021-10-18 21:45:13 -0400

    unit tests

--- shared/src/test/scala/io/github/edadma/recognizer/LinkParsingTests.scala
+++ shared/src/test/scala/io/github/edadma/recognizer/LinkParsingTests.scala
@@ -10,7 +10,7 @@ object grammar extends Testing {
   val ws1: Pattern = rep1(whitespace)
   lazy val balanced: Pattern = rep(noneOf('(', ')', ' ', '\n') | '(' ~ nonStrict(balanced) ~ ')')
   val link: Pattern =
-    '[' ~ string(rep(noneOf(']'))) ~ ']' ~ ws ~
+    '[' ~ string(rep(noneOf(']'))) ~ ']' ~
       '(' ~ ws ~
       ('<' ~ string(rep(noneOf('>', '\n'))) ~ '>' | not('<') ~ string(balanced)) ~
       opt(ws1 ~ ('"' ~ string(rep(noneOf('"'))) ~ '"' | '\'' ~ string(rep(noneOf('\''))) ~ '\'' | '(' ~ string(
@@ -98,7 +98,25 @@ class LinkParsingTests extends AnyFreeSpec with Matchers {
   }
 
   "link 20" in {
-    parse("[link](/url\"title\")", link) shouldBe None
+    parse("[link](/url\u00a0\"title\")", link) shouldBe Some(Some(Link("link", "/url\u00a0\"title\"", None)), "")
+  }
+
+  "link 21" in {
+    parse("[link](/url \"title \"and\" title\")", link) shouldBe None
+  }
+
+  "link 22" in {
+    parse("[link](/url 'title \"and\" title')", link) shouldBe Some(
+      Some(Link("link", "/url", Some("title \"and\" title"))),
+      "")
+  }
+
+  "link 23" in {
+    parse("[link](   /uri\n  \"title\"  )", link) shouldBe Some(Some(Link("link", "/uri", Some("title"))), "")
+  }
+
+  "link 24" in {
+    parse("[link] (/uri)", link) shouldBe None
   }
 
 }
